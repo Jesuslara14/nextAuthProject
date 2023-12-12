e8793d82c274e2a50617c6310c9d122b2fba78c2
Jesuslara14 <111470200+Jesuslara14@users.noreply.github.com>
2023-12-12 13:47:55 -0700

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,24 +1,29 @@
-import dbConnect from "@/utils/mongodb";
+import connect from "@/utils/mongodb";
 import Credentials from "next-auth/providers/credentials";
+import User from "@/models/user";
 import Github from "next-auth/providers/github"
 import NextAuth from "next-auth/next";
 import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
-    secret: 'f4b112163e220b2faacc746879d4a325ec9948c8',
     providers: [
         Credentials({
-            name: 'credentials',
-            credentials: {
-                email: { label: 'email', type: 'text' },
-                password: {  label: 'password', type: 'password' },
-            },
-            authorize: async (credentials) => {
-                const { db } = await dbConnect();
-                const user = await db.collection('users').findOne({ email: credentials.email });
-        
-                if (user && bcrypt.compareSync(credentials.password, user.passwordHash)) {
-                    return user
+            id: "credentials",
+            name: 'Credentials',
+            async authorize(credentials) {
+                await connect();
+                const user = await User.findOne({ email: credentials.email });
+
+                if (user) {
+                    const isPasswordCorrect = await bcrypt.compare(
+                        credentials.password,
+                        user.password
+                    );
+                    if (isPasswordCorrect) {
+                        return user;
+                      } else {
+                        throw new Error("Wrong Credentials!");
+                    }
                     //Promise.resolve(user);
                 } else {
                     return null
@@ -31,8 +36,7 @@ const handler = NextAuth({
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
         }),
     ],
-    database: process.env.MONGO_URL,
-    debug: true
+    database: process.env.MONGO_URL
 });
 
 export {handler as GET, handler as POST}
